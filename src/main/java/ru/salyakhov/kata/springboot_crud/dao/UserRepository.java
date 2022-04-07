97d23c78920647bc602c7c2ba42b1943af4b90f3
Rom4io <salyahov@gmail.com>
2022-04-07 14:27:13 +0300

    n/w extended DAO's

--- src/main/java/ru/salyakhov/kata/springboot_crud/dao/UserRepository.java
+++ src/main/java/ru/salyakhov/kata/springboot_crud/dao/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.salyakhov.kata.springboot_crud.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
